e8e3177446c1f5a5ed82a712596287ecf1545bb4
xiangshouding <fansekey@gmail.com>
2015-06-04 01:57:57 +0000

    based on https://github.com/fis-components/components/blob/master/modules/rosetta.js

---
+++ index.js
@@ -0,0 +1,20 @@
+var Rosetta = require('./lib/rosetta.js'),
+
+    readyRE = /complete/,
+    ready = function(callback) {
+        if (readyRE.test(document.readyState) && document.body) {
+            callback();
+        } else {
+            if (!document.addEventListener) {
+                window.attachEvent('onload', callback);
+            } else {
+                document.addEventListener('DOMContentLoaded', function() {
+                    callback();
+                }, false);
+            }
+        }
+    };
+
+window.Rosetta = Rosetta;
+
+ready(Rosetta.init);
